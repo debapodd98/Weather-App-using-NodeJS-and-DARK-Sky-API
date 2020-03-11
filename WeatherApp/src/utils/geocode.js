4d148751437c647c0d67539b6b87a6213d70d8cc
Debanjan Podder <47063998+debapodd98@users.noreply.github.com>
2020-03-11 15:32:10 +0530

    Update geocode.js

--- WeatherApp/src/utils/geocode.js
+++ WeatherApp/src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZGViYXBvZGQxOSIsImEiOiJjanhxNW1uMWwwamJ3M2hsM2JwdGZhbDIyIn0.2Dp6L2ChhZlAFj1LL64dqg&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=<your token>&limit=1'
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to Location Services!', undefined)
